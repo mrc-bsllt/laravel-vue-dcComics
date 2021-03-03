f325e43d4cb86288c5a6cbedb0a5f0f408d07f0e
Simone Romiti <sr1992ei@gmail.com>
2021-03-03 22:14:48 +0100

    conflitto risolto

--- app/User.php
+++ app/User.php
@@ -37,9 +37,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-<<<<<<< HEAD
+
     protected $timestamps = false;
-=======
-    public $numero = 5;
->>>>>>> Branch-Simone
+ 
 }
